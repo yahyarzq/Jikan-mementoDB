6db1f9fbc1341af3e23290be756a551abc51d76c
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-02-20 15:26:38 +0700

    Update jikan-mementoDB.js

--- jikan-mementoDB.js
+++ jikan-mementoDB.js
@@ -10,7 +10,7 @@ The data source for obtaining information from Jikan.moe.
 @example 
 var jikan = new Jikan( "anime" );
 var r = jikan.search(query);
-result( r , function(id) { return discogs.extra(id);});
+result( r , function(id) { return Jikan.extra(id);});
 */
 function Jikan (type) {
     this.type = type;
@@ -18,7 +18,7 @@ function Jikan (type) {
 
 
 /**
-Issue a search query to Discogs database.
+Issue a search query to Jikan database.
 @param {string} query - Search query.
 */
 var baseurl = "https://api.jikan.moe/v4/";
@@ -29,14 +29,71 @@ Jikan.prototype.search = function(query) {
   return json.data;  
 }
 
-
 /**
 @param {string} id - The resource identifier.
 */
 Jikan.prototype.extra = function(id) {
     var resultJson = http().get(baseurl + this.type + "/" + id);
-    var result = JSON.parse(resultJson.body); 
-          
+    var res = JSON.parse(resultJson.body);
+    var result = {};
+    if (res.data.mal_id !== undefined) {
+        result["mal_id"] = res.data.mal_id;
+    }
+    if (res.data.title !== undefined) {
+        result["url"] = res.data.url;
+    }
+    if (res.data.images.jpg.image_url !== undefined) {
+        result["image"] = res.data.images.jpg.image_url;
+    }
+    if (res.data.title !== undefined) {
+        result["title"] = res.data.title;
+    }
+    if (res.data.title_english !== undefined) {
+        result["title_english"] = res.data.title_english;
+    }
+    if (res.data.title_japanese !== undefined) {
+        result["title_japanese"] = res.data.title_japanese;
+    }
+    if (res.data.type !== undefined) {
+        result["type"] = res.data.type;
+    }
+    if (res.data.source !== undefined) {
+        result["source"] = res.data.source;
+    }
+    if (res.data.episodes !== undefined) {
+        result["episodes"] = res.data.episodes;
+    }
+    if (res.data.status !== undefined) {
+        result["status"] = res.data.status;
+    }
+    if (res.data.aired.string !== undefined) {
+        result["aired"] = res.data.aired.string;
+    }
+    if (res.data.duration !== undefined) {
+        result["duration"] = res.data.duration;
+    }
+    if (res.data.rating !== undefined) {
+        result["rating"] = res.data.rating;
+    }
+    if (res.data.score !== undefined) {
+        result["score"] = res.data.score;
+    }
+    if (res.data.scored_by !== undefined) {
+        result["scored_by"] = res.data.scored_by;
+    }
+    if (res.data.popularity !== undefined) {
+        result["popularity"] = res.data.popularity;
+    }
+    if (res.data.members !== undefined) {
+        result["members"] = res.data.members;
+    }
+    if (res.data.favorites !== undefined) {
+        result["favorites"] = res.data.favorites;
+    }
+    if (res.data.synopsis !== undefined) {
+        result["synopsis"] = res.data.synopsis;
+    }
+    
     return result;
 }
 
