323e81370f8482dcdbe4f3d6aa6e9fffd76d9e2f
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-02-20 13:51:50 +0700

    Update jikan-mementoDB.js

--- jikan-mementoDB.js
+++ jikan-mementoDB.js
@@ -25,7 +25,7 @@ var baseurl = "https://api.jikan.moe/v4/";
 
 Jikan.prototype.search = function(query) {
   var result = http().get(baseurl + this.type + "?q="+ encodeURIComponent(query));
-  var json = JSON.parse(result.body).data;
+  var json = JSON.parse(result.body);
   return json.results;  
 }
 
