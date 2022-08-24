4ed7de16899f9bb9d4519e275b391f90c7ffc9ff
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-08-24 14:47:50 +0700

    Update

--- jikan-mementoDB.js
+++ jikan-mementoDB.js
@@ -41,68 +41,68 @@ Jikan.prototype.extra = function(id) {
     var resultJson = http().get(baseurl + this.type + "/" + id);
     var response = JSON.parse(resultJson);
     var res = response.body
-    var result = {};
-    if (res.data.mal_id !== undefined) {
-        result["mal_id"] = res.data.mal_id;
-    };
-    if (res.data.url !== undefined) {
-        result["url"] = res.data.url;
-    };
-    if (res.data.images.jpg.image_url !== undefined) {
-        result["image"] = res.data.images.jpg.image_url;
-    };
-    if (res.data.title !== undefined) {
-        result["title"] = res.data.title;
-    };
-    if (res.data.title_english !== undefined) {
-        result["title_english"] = res.data.title_english;
-    };
-    if (res.data.title_japanese !== undefined) {
-        result["title_japanese"] = res.data.title_japanese;
-    };
-    if (res.data.type !== undefined) {
-        result["type"] = res.data.type;
-    };
-    if (res.data.source !== undefined) {
-        result["source"] = res.data.source;
-    };
-    if (res.data.episodes !== undefined) {
-        result["episodes"] = res.data.episodes;
-    };
-    if (res.data.status !== undefined) {
-        result["status"] = res.data.status;
-    };
-    if (res.data.aired.string !== undefined) {
-        result["aired"] = res.data.aired.string;
-    };
-    if (res.data.duration !== undefined) {
-        result["duration"] = res.data.duration;
-    };
-    if (res.data.rating !== undefined) {
-        result["rating"] = res.data.rating;
-    };
-    if (res.data.score !== undefined) {
-        result["score"] = res.data.score;
-    };
-    if (res.data.scored_by !== undefined) {
-        result["scored_by"] = res.data.scored_by;
-    };
-    if (res.data.popularity !== undefined) {
-        result["popularity"] = res.data.popularity;
-    };
-    if (res.data.members !== undefined) {
-        result["members"] = res.data.members;
-    };
-    if (res.data.favorites !== undefined) {
-        result["favorites"] = res.data.favorites;
-    };
-    if (res.data.synopsis !== undefined) {
-        result["synopsis"] = res.data.synopsis;
-    };
-    if (res.data !== undefined) {
-        result["resbody"] = res.data;
-    };
-    return result;
+    // var result = {};
+    // if (res.data.mal_id !== undefined) {
+    //     result["mal_id"] = res.data.mal_id;
+    // };
+    // if (res.data.url !== undefined) {
+    //     result["url"] = res.data.url;
+    // };
+    // if (res.data.images.jpg.image_url !== undefined) {
+    //     result["image"] = res.data.images.jpg.image_url;
+    // };
+    // if (res.data.title !== undefined) {
+    //     result["title"] = res.data.title;
+    // };
+    // if (res.data.title_english !== undefined) {
+    //     result["title_english"] = res.data.title_english;
+    // };
+    // if (res.data.title_japanese !== undefined) {
+    //     result["title_japanese"] = res.data.title_japanese;
+    // };
+    // if (res.data.type !== undefined) {
+    //     result["type"] = res.data.type;
+    // };
+    // if (res.data.source !== undefined) {
+    //     result["source"] = res.data.source;
+    // };
+    // if (res.data.episodes !== undefined) {
+    //     result["episodes"] = res.data.episodes;
+    // };
+    // if (res.data.status !== undefined) {
+    //     result["status"] = res.data.status;
+    // };
+    // if (res.data.aired.string !== undefined) {
+    //     result["aired"] = res.data.aired.string;
+    // };
+    // if (res.data.duration !== undefined) {
+    //     result["duration"] = res.data.duration;
+    // };
+    // if (res.data.rating !== undefined) {
+    //     result["rating"] = res.data.rating;
+    // };
+    // if (res.data.score !== undefined) {
+    //     result["score"] = res.data.score;
+    // };
+    // if (res.data.scored_by !== undefined) {
+    //     result["scored_by"] = res.data.scored_by;
+    // };
+    // if (res.data.popularity !== undefined) {
+    //     result["popularity"] = res.data.popularity;
+    // };
+    // if (res.data.members !== undefined) {
+    //     result["members"] = res.data.members;
+    // };
+    // if (res.data.favorites !== undefined) {
+    //     result["favorites"] = res.data.favorites;
+    // };
+    // if (res.data.synopsis !== undefined) {
+    //     result["synopsis"] = res.data.synopsis;
+    // };
+    // if (res.data !== undefined) {
+    //     result["resbody"] = res.data;
+    // };
+    return res;
 }
 
 
